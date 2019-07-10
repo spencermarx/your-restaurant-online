9ab18233d50ac467fc495d3c57ee60fb9234c4a9
Spencer Marx <spencer.s.marx@gmail.com>
2019-07-10 11:23:53 -0400

    Add background to navigation when hamburger clicked at top of page

--- src/js/0-Components/hamburger/hamburger.js
+++ src/js/0-Components/hamburger/hamburger.js
@@ -11,6 +11,18 @@ hamburger.addEventListener(
   function() {
     // Toggle '"is-active" class
     this.classList.toggle('is-active');
+
+    // Check scroll position
+    const scrolled = navScrollCheck();
+
+    // If at the top of the page add a solid background to the navbar
+    if (!scrolled) {
+      navigation.classList.toggle('sm--navbar__clear');
+      navigation.classList.toggle('sm--navbar__solid');
+    } else {
+      navigation.classList.toggle('sm--navbar__solid');
+      navigation.classList.toggle('sm--navbar__clear');
+    }
   },
   false
 );
