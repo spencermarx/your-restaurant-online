66739ccd73d5f749a501f050ec448109c5297e2b
Spencer Marx <spencer.s.marx@gmail.com>
2019-07-16 13:38:33 -0400

    Initial Let's Encrypt file for serving to gain SSL certification

--- controllers/index.js
+++ controllers/index.js
@@ -11,4 +11,14 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+// SSL Verification
+app.get(
+  '/.well-known/acme-challenge/YMharxgU-_sG47xBMtKRcLNe2rQQ8rvMg8YZ9XabfMs',
+  function(req, res) {
+    res.send(
+      'YMharxgU-_sG47xBMtKRcLNe2rQQ8rvMg8YZ9XabfMs.o_97jTnYZ7-JvFgisMKx0sRWdIxyq7T2KStlQ-McVUY'
+    );
+  }
+);
+
 module.exports = router;
