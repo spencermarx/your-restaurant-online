d20baf7bd9b921ea51c785c79ab06269ef0d798f
Spencer Marx <spencer.s.marx@gmail.com>
2019-07-18 16:06:17 -0400

    Add scroll-based active navbar links

--- src/js/0-Components/hamburger/hamburger.js
+++ src/js/0-Components/hamburger/hamburger.js
@@ -5,24 +5,33 @@
 // Select Hamburger
 const hamburger = document.querySelector('.hamburger');
 
+// Change navbar backgrounds
+function toggleNavbarBackgrounds() {
+  navigation.classList.toggle('sm--navbar__clear');
+  navigation.classList.toggle('sm--navbar__solid');
+}
+
+// Handle Navbar State
+function handleNavbarState() {
+  // Toggle '"is-active" class
+  hamburger.classList.toggle('is-active');
+
+  // Check scroll position
+  const scrolled = navScrollCheck();
+
+  // If at the top of the page add a solid background to the navbar
+  if (!scrolled) {
+    toggleNavbarBackgrounds();
+  } else {
+    toggleNavbarBackgrounds();
+  }
+}
+
 // Add event listener
 hamburger.addEventListener(
   'click',
   function() {
-    // Toggle '"is-active" class
-    this.classList.toggle('is-active');
-
-    // Check scroll position
-    const scrolled = navScrollCheck();
-
-    // If at the top of the page add a solid background to the navbar
-    if (!scrolled) {
-      navigation.classList.toggle('sm--navbar__clear');
-      navigation.classList.toggle('sm--navbar__solid');
-    } else {
-      navigation.classList.toggle('sm--navbar__solid');
-      navigation.classList.toggle('sm--navbar__clear');
-    }
+    handleNavbarState();
   },
   false
 );
